2836983a04d9bda4896267a4dd3db3e8eb76c7e6
Evan Hemsley <evan.hemsley@gmail.com>
2013-06-10 11:58:39 -0500

    slight change to recommendations

--- app/engines/recommendations.rb
+++ app/engines/recommendations.rb
@@ -1,7 +1,18 @@
 class Recommendations
+
+  def self.best_pick list_of_heroes
+    original_list = list_of_heroes 
+    remaining_heroes = Hero.all.select{ |hero| !original_list.include?(hero) }
+    scores = []
+    remaining_heroes.each do |hero|
+      scores << [hero.name, score(list_of_heroes + [hero])]
+    end
+    scores
+  end
+
   #given list of heroes
   #returns best pick
-  def self.best_pick list_of_heroes
+  def self.score list_of_heroes
     role_values = {
       :lane_support => 0,
       :carry => 0,
@@ -24,6 +35,14 @@ class Recommendations
       end
     end
 
-    role_values
+    roles_filled = role_values.select { |role_name, value| value > 0 }.count
+    
+    support_bonus = false
+    carry_bonus = false
+
+    support_bonus = true if (role_values[:support] + role_values[:lane_support]) > 4
+    carry_bonus = true if role_values[:carry] < 4
+
+    score = roles_filled + ((support_bonus ? 1 : 0) * 10) + ((carry_bonus ? 1 : 0) * 10)
   end
 end
