79103605eef8b11882d610c1e9d4ceadfea99767
Evan Hemsley <evan.hemsley@gmail.com>
2013-12-10 23:25:43 -0800

    started adding new heroes

--- app/engines/recommendations.rb
+++ app/engines/recommendations.rb
@@ -2,15 +2,14 @@ class Recommendations
   TARGET_VECTOR = Vector.elements([3, 4, 3, 3, 1, 2, 3, 2, 2, 3, 2, 3])
 
   class << self
-    #given list of heroes
-    #returns top five picks for team and worst five picks
     #lower score is better
     def pick_recommendations friendly_heroes, enemy_heroes
       remaining_heroes = remaining_heroes(friendly_heroes, enemy_heroes)
 
-      scores = []
-      remaining_heroes.each do |hero|
-        scores << [hero, calculate_score(friendly_heroes + [hero])]
+      scores = [].tap do |score_builder|
+        remaining_heroes.each do |hero|
+          score_builder << [hero, calculate_score(friendly_heroes + [hero])]
+        end
       end
 
       scores = scores.sort_by { |a, b| b }
@@ -24,66 +23,9 @@ class Recommendations
       Hero.where.not(name: (friendly_heroes + enemy_heroes).map(&:name))
     end
 
-    def heroes_to_check remaining_heroes, role_values
-      heroes = []
-      heroes += Hero.carries.sample(3) if role_values[:carry] < 3
-      heroes += Hero.supports.sample(3) if role_values[:support] + role_values[:lane_support] < 5
-      heroes += Hero.pushers.sample(3) if role_values[:pusher] < 1
-      heroes += Hero.initiators.sample(3) if role_values[:initiator] < 1
-      heroes += Hero.disablers.sample(3) if role_values[:disabler] < 1
-      heroes += Hero.gankers.sample(3) if role_values[:ganker] < 1
-      heroes += Hero.junglers.sample(3) if role_values[:jungler] < 1
-      heroes += Hero.durables.sample(3) if role_values[:durable] < 1
-      heroes && remaining_heroes
-    end
-
     def calculate_score friendly_heroes
       team_vector = friendly_heroes.map(&:role_vector).inject(:+)
       (TARGET_VECTOR - team_vector).map {|x| x * x }.inject(:+)
-    end
-
-    # def calculate_score friendly_heroes, enemy_heroes, role_values
-    #   [roles_filled_score(role_values),
-    #     support_bonus(role_values[:support],role_values[:lane_support]),
-    #     carry_bonus(role_values[:carry]),
-    #     melee_and_ranged_bonus(friendly_heroes),
-    #     disabler_bonus(role_values[:disabler]),
-    #     initiator_bonus(role_values[:initiator]),
-    #     durable_bonus(role_values[:durable]),
-    #     mid_bonus(friendly_heroes),
-    #     counter_value(friendly_heroes, enemy_heroes)].inject(:+)
-    # end
-
-    def roles_filled_score role_values
-      role_values.select { |role_name, value| value > 0 }.count * 2
-    end
-
-    def melee_and_ranged_bonus friendly_heroes
-      friendly_heroes.map(&:attack_type).count{ |type| type == :ranged } > 1 ? 5 : 0
-    end
-
-    def support_bonus support_value, lane_support_value
-      (support_value + lane_support_value) > 4 ? 10 : 0
-    end
-
-    def carry_bonus carry_value
-      carry_bonus = 0
-      carry_bonus = 30 if carry_value > 0
-      carry_bonus -= (carry_value * 5) if carry_value > 5
-      carry_bonus
-    end
-
-    #should maybe compare disabler values of two teams: having more stun is important
-    def disabler_bonus disabler_value
-      disabler_value > 0 ? 5 : 0
-    end
-
-    def initiator_bonus initiator_value
-      initiator_value > 0 ? 5 : 0
-    end
-
-    def durable_bonus durable_value
-      durable_value > 0 ? 5 : 0
     end
 
     def mid_bonus friendly_heroes
