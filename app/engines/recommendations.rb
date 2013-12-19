8b6a65344c8a70450087268db6bb76f182e1513d
Evan Hemsley <evan.hemsley@gmail.com>
2013-12-19 11:32:06 -0800

    added ability to select different styles of team composition

--- app/engines/recommendations.rb
+++ app/engines/recommendations.rb
@@ -1,14 +1,16 @@
 module Recommendations
-  IDEAL_VECTOR = Vector.elements([3, 1, 3, 5, 3, 1, 2, 4, 1, 2, 3, 2, 3])
+  IDEAL_BALANCED_VECTOR = Vector.elements([3, 1, 3, 5, 3, 1, 2, 4, 0, 2, 3, 2, 3])
+  IDEAL_JUNGLING_VECTOR = Vector.elements([3, 2, 3, 4, 3, 1, 1, 3, 3, 3, 2, 2, 2])
+  IDEAL_TRILANE_VECTOR = Vector.elements([3, 2, 3, 5, 3, 1, 3, 3, 0, 3, 3, 1, 2])
 
   class << self
     #lower score is better
-    def pick_recommendations friendly_heroes, enemy_heroes
+    def pick_recommendations friendly_heroes, enemy_heroes, ideal_composition_vector
       remaining_heroes = remaining_heroes(friendly_heroes, enemy_heroes)
 
       scores = [].tap do |score_builder|
         remaining_heroes.each do |hero|
-          score_builder << [hero, calculate_score(friendly_heroes + [hero])]
+          score_builder << [hero, calculate_score(friendly_heroes + [hero], ideal_composition_vector)]
         end
       end.sort_by { |a, b| b }
 
@@ -19,9 +21,9 @@ module Recommendations
       Hero.where.not(id: (friendly_heroes.map(&:id) + enemy_heroes.map(&:id)))
     end
 
-    def calculate_score friendly_heroes
+    def calculate_score friendly_heroes, ideal_composition_vector
       team_vector = friendly_heroes.map(&:role_vector).inject(:+)
-      (IDEAL_VECTOR - team_vector).map {|x| x * x }.inject(:+)
+      (const_get(ideal_composition_vector) - team_vector).map {|x| x * x }.inject(:+)
     end
   end
 end
